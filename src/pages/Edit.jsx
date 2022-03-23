0068514d548a968ced60b8ee6f18609d6cc30e74
Skylar <skylar090807@gmail.com>
2022-03-23 16:45:13 +0900

    상세페이지 Query String useSearchParams() 사용

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom'
 
 const Edit = () => {
-  return <div>Edit</div>
+  //Query String useSearchParams()
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const id = searchParams.get(`id`)
+  console.log('id :', id)
+
+  const mode = searchParams.get(`mode`)
+  console.log('mode :', mode)
+
+  const SearchParamsHandler = () => {
+    setSearchParams({
+      who: 'skylar',
+      id: 10,
+      mode: 'dark',
+    })
+  }
+
+  return (
+    <div>
+      <h1>Edit</h1>
+      <p>일기 작성 페이지</p>
+      <button onClick={SearchParamsHandler}>Query String 변경</button>
+    </div>
+  )
 }
 
 export default Edit
