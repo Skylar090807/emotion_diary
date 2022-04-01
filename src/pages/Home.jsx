8e1d91557990a2e67e1673239195b6787635de78
Skylar <skylar090807@gmail.com>
2022-04-01 16:52:52 +0900

    Home 페이지 Header 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Header from '../components/Header'
+import Button from '../components/Button'
 
 const Home = () => {
-  return <div>Home</div>
+  const [curDate, setCurDate] = useState(new Date())
+  console.log(curDate)
+
+  //getMonth는 0월 부터 시작해서 +1 해줘야 한다.
+  const headerTxt = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`
+
+  const increaseMonth = () => {
+    setCurDate(new Date(curDate.getFullYear(), curDate.getMonth() + 1, curDate.getDate()))
+  }
+
+  const decreseMonth = () => {
+    setCurDate(new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate()))
+  }
+  return (
+    <div>
+      <Header
+        headText={headerTxt}
+        leftChild={<Button text={'<'} onClick={decreseMonth} />}
+        rightChild={<Button text={'>'} onClick={increaseMonth} />}
+      />
+    </div>
+  )
 }
 
 export default Home
