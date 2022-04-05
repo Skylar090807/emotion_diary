5ef1faf768bbd60d8620454c47f46f00f4e95040
Skylar <skylar090807@gmail.com>
2022-04-05 18:52:12 +0900

    수정하기 버튼 생성, 상세페이지로 페이지 이동

--- src/components/DiaryItem.jsx
+++ src/components/DiaryItem.jsx
@@ -1,21 +1,34 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import Button from './Button'
 
 const DiaryItem = ({ id, emotion, content, date }) => {
+  const navigate = useNavigate()
+
   const env = process.env
   env.PUBLIC_URL = env.PUBLIC_URL || ''
 
   const strDate = new Date(parseInt(date)).toLocaleDateString()
 
+  const goDetail = () => {
+    navigate(`/diary/${id}`)
+  }
+
+  const goEdit = () => {
+    navigate(`/edit/${id}`)
+  }
   return (
     <div className="DiaryItem">
-      <div className={['emotion_img_wrap', `emtion_img_wrapper_${emotion}`].join(' ')}>
+      <div onClick={goDetail} className={['emotion_img_wrap', `emtion_img_wrapper_${emotion}`].join(' ')}>
         <img src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`} alt="" />
       </div>
-      <div className="info_wrap">
+      <div onClick={goDetail} className="info_wrap">
         <div className="diary_date">{strDate}</div>
         <div className="diary_content_preview">{content.slice(0, 25)}</div>
       </div>
-      <div></div>
+      <div className="btn_wrap">
+        <Button text={'수정하기'} type={'negative'} onClick={goEdit} />
+      </div>
     </div>
   )
 }
