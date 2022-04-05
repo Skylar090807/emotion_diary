c43bf4f4cb0744ca30e1efd7c20d001420972505
Skylar <skylar090807@gmail.com>
2022-04-05 18:18:27 +0900

    DiaryItem date와 content불러오기, DiaryItem 스타일 수정

--- src/components/DiaryItem.jsx
+++ src/components/DiaryItem.jsx
@@ -4,12 +4,17 @@ const DiaryItem = ({ id, emotion, content, date }) => {
   const env = process.env
   env.PUBLIC_URL = env.PUBLIC_URL || ''
 
+  const strDate = new Date(parseInt(date)).toLocaleDateString()
+
   return (
     <div className="DiaryItem">
       <div className={['emotion_img_wrap', `emtion_img_wrapper_${emotion}`].join(' ')}>
         <img src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`} alt="" />
       </div>
-      <div></div>
+      <div className="info_wrap">
+        <div className="diary_date">{strDate}</div>
+        <div className="diary_content_preview">{content.slice(0, 25)}</div>
+      </div>
       <div></div>
     </div>
   )
