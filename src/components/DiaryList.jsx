bf8e09bdbcf1759dbe0f0cbc30d345c4abb00674
Skylar <skylar090807@gmail.com>
2022-04-05 16:31:41 +0900

    DiaryList 컴포넌트 스타일 적용

--- src/components/DiaryList.jsx
+++ src/components/DiaryList.jsx
@@ -19,7 +19,7 @@ const ControlMenu = ({ value, onChange, optionList }) => {
   //onChange prop: select가 변경되었을 때 바꿀 기능을 하는 함수.
   //optionList prop: select 태그 안의 options.
   return (
-    <select value={value} onChange={(e) => onChange(e.target.value)}>
+    <select className="ControlMenu" value={value} onChange={(e) => onChange(e.target.value)}>
       {optionList.map((it, idx) => (
         <option key={idx} value={it.value}>
           {it.name}
@@ -66,13 +66,19 @@ const DiaryList = ({ diaryList }) => {
   }
 
   return (
-    <div>
-      {/* controlMenu의 value prop역할은 정렬기준이 되는 sortType을 변화시키는 select의 역할을 하기때문에 value에 sortType을 내려준다. */}
-      {/* onChange prop의 역할은 select가 변화 되었을 때 수행되어야 하므로 setSortType을 내려준다. */}
-      {/* optionList prop의 역할은 어떤 일기인지 option을 보여주는 것이므로 일기 종류를 배열로 담고있는 sortOptionList를 내려준다. */}
-      <ControlMenu value={sortType} onChange={setSortType} optionList={sortOptionList} />
-      <ControlMenu value={emotionFilter} onChange={setEmotionFilter} optionList={emotionFilterOptionList} />
-      <Button type={'positive'} text={'새로운 일기'} onClick={() => navigate('/new')} />
+    // controlMenu의 value prop역할은 정렬기준이 되는 sortType을 변화시키는 select의 역할을 하기때문에 value에 sortType을 내려준다.
+    // onChange prop의 역할은 select가 변화 되었을 때 수행되어야 하므로 setSortType을 내려준다.
+    // optionList prop의 역할은 어떤 일기인지 option을 보여주는 것이므로 일기 종류를 배열로 담고있는 sortOptionList를 내려준다.
+    <div className="DiaryList">
+      <div className="menu_wrapper">
+        <div className="left_col">
+          <ControlMenu value={sortType} onChange={setSortType} optionList={sortOptionList} />
+          <ControlMenu value={emotionFilter} onChange={setEmotionFilter} optionList={emotionFilterOptionList} />
+        </div>
+        <div className="right_col">
+          <Button type={'positive'} text={'새로운 일기'} onClick={() => navigate('/new')} />
+        </div>
+      </div>
       {getProcessedDiaryList().map((it) => (
         <div key={it.id}>
           {it.content}
