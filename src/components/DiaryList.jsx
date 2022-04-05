8c611fda9dcc5563679cfb92e2e574e9ab2d51d1
Skylar <skylar090807@gmail.com>
2022-04-05 14:57:09 +0900

    getProcessedDiaryList 함수 사용.

--- src/components/DiaryList.jsx
+++ src/components/DiaryList.jsx
@@ -28,6 +28,7 @@ const DiaryList = ({ diaryList }) => {
 
   //ControlMenu option 선택 시 해당 일기 보여주는 기능 구현.
 
+  //diaryList를 최신 순으로 정렬하는 함수 getProcessedDiaryList 구현.
   //배열 diaryList prop 깊은 복사!
   //diaryList.sort()를 사용하면 기존 배열 자체가 변경되기 때문에
   // 깊은 복사를 한 후 JSON.parse(JSON.stringify())함수를 사용해 데이터를 붙여 넣어 주고 copyList에 대입한다.
@@ -42,17 +43,16 @@ const DiaryList = ({ diaryList }) => {
 
     const copyList = JSON.parse(JSON.stringify(diaryList))
     const sortedList = copyList.sort(compare)
-    console.log(sortedList)
+    return sortedList
   }
 
-  getProcessedDiaryList()
   return (
     <div>
       {/* controlMenu의 value prop역할은 정렬기준이 되는 sortType을 변화시키는 select의 역할을 하기때문에 value에 sortType을 내려준다. */}
       {/* onChange prop의 역할은 select가 변화 되었을 때 수행되어야 하므로 setSortType을 내려준다. */}
       {/* optionList prop의 역할은 어떤 일기인지 option을 보여주는 것이므로 일기 종류를 배열로 담고있는 sortOptionList를 내려준다. */}
       <ControlMenu value={sortType} onChange={setSortType} optionList={sortOptionList} />
-      {diaryList.map((it) => (
+      {getProcessedDiaryList().map((it) => (
         <div key={it.id}>{it.content}</div>
       ))}
     </div>
