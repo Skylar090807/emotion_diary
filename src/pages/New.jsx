51155c539c46b43d06e23553e1b30459c7b148ea
Skylar <skylar090807@gmail.com>
2022-04-06 11:33:38 +0900

    캘린더에 오늘의 날짜 default로 띄우기

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -4,15 +4,26 @@ import Button from '../components/Button'
 import Header from '../components/Header'
 
 // '오늘의 날짜 선택' 캘린더에서 오늘의 날짜가 default로 떠있게 하기 위해서는 new Date()객체를 함수
-// 안에서 가공 해준 후 useState() 초기값으로 할당해줘야 한다.
+// 안에서 .toISOString().slice(0, 10)로 가공 해준 후 useState() 초기값으로 할당해줘야 한다.
 const getStringDate = (date) => {
   return date.toISOString().slice(0, 10)
+  //toISOString은 new Date()객체의 메서드.
+  //toISOString() 출력
+  // 2022-04-06T02:07:07.202Z
+
+  //date.toISOString().slice(0, 10) 출력
+  // 2022-04-06
 }
 
 const New = () => {
+  //getStringDate 출력 확인
+  console.log(getStringDate(new Date()))
+  //getStringDate에 new Date()를 할당한 것과 아래는 동일하다.
+  console.log(new Date().toISOString().slice(0, 10))
+
   const navigate = useNavigate()
 
-  const [date, setDate] = useState()
+  const [date, setDate] = useState(getStringDate(new Date()))
 
   const pagebackHandler = () => {
     navigate(-1)
@@ -24,7 +35,7 @@ const New = () => {
         <section>
           <h4>오늘의 날짜 선택</h4>
           <div className="input-box">
-            <input type="date" onChange={(e) => setDate(e.target.value)} />
+            <input value={date} type="date" onChange={(e) => setDate(e.target.value)} />
           </div>
         </section>
       </div>
