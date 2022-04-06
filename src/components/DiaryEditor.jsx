decad1fb0ca61c6885eb9c28a59df89baf9ac8b0
Skylar <skylar090807@gmail.com>
2022-04-06 12:43:32 +0900

    공통으로 사용하는 코드 DiaryEditor 컴포넌트로 재사용하기. DiaryEditor에 emotion 배열 만들고 사용하기

---
+++ src/components/DiaryEditor.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Button from './Button'
+import Header from './Header'
+
+const env = process.env
+env.PUBLIC_URL = env.PUBLIC_URL || ''
+
+const emotionList = [
+  {
+    emotion_id: 1,
+    emotion_img: process.env.PUBLIC_URL + `assets/emotion1.png`,
+    emotion_descript: '행복해',
+  },
+  {
+    emotion_id: 2,
+    emotion_img: process.env.PUBLIC_URL + `assets/emotion2.png`,
+    emotion_descript: '즐거워',
+  },
+  {
+    emotion_id: 3,
+    emotion_img: process.env.PUBLIC_URL + `assets/emotion3.png`,
+    emotion_descript: '나쁘지 않아',
+  },
+  {
+    emotion_id: 4,
+    emotion_img: process.env.PUBLIC_URL + `assets/emotion4.png`,
+    emotion_descript: '짜증나',
+  },
+  {
+    emotion_id: 5,
+    emotion_img: process.env.PUBLIC_URL + `assets/emotion5.png`,
+    emotion_descript: '최악이야',
+  },
+]
+
+// '오늘의 날짜 선택' 캘린더에서 오늘의 날짜가 default로 떠있게 하기 위해서는 new Date()객체를 함수
+// 안에서 .toISOString().slice(0, 10)로 가공 해준 후 useState() 초기값으로 할당해줘야 한다.
+const getStringDate = (date) => {
+  return date.toISOString().slice(0, 10)
+  //toISOString은 new Date()객체의 메서드.
+  //toISOString() 출력
+  // 2022-04-06T02:07:07.202Z
+
+  //date.toISOString().slice(0, 10) 출력
+  // 2022-04-06
+}
+
+const DiaryEditor = () => {
+  //getStringDate 출력 확인
+  console.log(getStringDate(new Date()))
+  //getStringDate에 new Date()를 할당한 것과 아래는 동일하다.
+  console.log(new Date().toISOString().slice(0, 10))
+
+  const navigate = useNavigate()
+
+  const [date, setDate] = useState(getStringDate(new Date()))
+
+  const pagebackHandler = () => {
+    navigate(-1)
+  }
+  return (
+    <div className="DiaryEditor">
+      <Header headText={'새로운 일기'} leftChild={<Button text={'< 뒤로가기'} onClick={pagebackHandler} />} />
+      <div>
+        <section>
+          <h4>오늘의 날짜 선택</h4>
+          <div className="input_box">
+            <input className="input_date" value={date} type="date" onChange={(e) => setDate(e.target.value)} />
+          </div>
+        </section>
+        <section>
+          <h4>오늘 나의 감정</h4>
+          <div className="input_box emotion_list_wrap">
+            {emotionList.map((it) => (
+              <div key={it.emotion_id}>{it.emotion_descript}</div>
+            ))}
+          </div>
+        </section>
+      </div>
+    </div>
+  )
+}
+
+export default DiaryEditor
